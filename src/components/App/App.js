74ce72c295bab28ee5f8406b78b807b759844076
Kate Danilenko <keitilins@yandex.ru>
2023-04-26 18:03:54 +0300

    header

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.module.css';
+
+import AppHeader from '../AppHeader/AppHeader';
+import Main from '../Main/Main';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import mainStyles from './Main.module.css';
+
+function Main() {
+  return (
+    <section className={mainStyles.main}>
+      
+    </section>
+  )
+}
+
+export default Main;
