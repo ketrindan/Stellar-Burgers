1dabe7d52d0a078f8c4b519e95b1cda0c6646f84
Ekaterina Danilenko <keitilins@yandex.ru>
2023-10-17 22:13:13 +0300

    adaptive header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { NavLink, useMatch } from 'react-router-dom';
 import { FC } from 'react';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import logo_mini from '../../images/logo.svg';
 
 import headerStyles from './AppHeader.module.css';
 
@@ -9,6 +11,17 @@ const AppHeader: FC = () => {
   const feed = useMatch('/feed/*');
   const profile = useMatch('/profile/*');
 
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  function handleBurgerClick() {
+    setMenuOpen((state) => !state);
+  }
+
+  function toggleBurgerClass() {
+    return isMenuOpen ? `${headerStyles.burger_btn} ${headerStyles.burger_btn_transformed}` : `${headerStyles.burger_btn}`
+  }
+
+
   return (
     <header className={`pt-4 pb-4 ${headerStyles.header}`}>
       <div className={headerStyles.container}>
@@ -16,12 +29,12 @@ const AppHeader: FC = () => {
           <NavLink end to='/' className={({ isActive }) => isActive ? `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link} ${headerStyles.link_active}` : 
               `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link}`} >
             <BurgerIcon  type={burgers ? "primary" : "secondary"}/>
-            <span className="text text_type_main-default ml-2">Конструктор</span>
+            <span className={`${headerStyles.span} text text_type_main-default ml-2`}>Конструктор</span>
           </NavLink>
           <NavLink to='/feed' className={({ isActive }) => isActive ? `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link} ${headerStyles.link_active}` : 
               `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link}`} >
             <ListIcon type={feed ? "primary" : "secondary"} />
-            <span className="text text_type_main-default ml-2">Лента заказов</span>
+            <span className={`${headerStyles.span} text text_type_main-default ml-2`}>Лента заказов</span>
           </NavLink>
         </nav>
         <div className={headerStyles.logo}>
@@ -30,8 +43,14 @@ const AppHeader: FC = () => {
         <NavLink to='/profile' className={({ isActive }) => isActive ? `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link} ${headerStyles.profile} ${headerStyles.link_active}` : 
               `pt-4 pl-5 pb-4 pr-5 ${headerStyles.link} ${headerStyles.profile}`} >
           <ProfileIcon type={profile ? "primary" : "secondary"} />
-          <span className="text text_type_main-default ml-2">Личный кабинет</span>
+          <span className={`${headerStyles.span} text text_type_main-default ml-2`}>Личный кабинет</span>
         </NavLink>
+      </div>
+      <img src={logo_mini} alt='логотип' className={headerStyles.logo_mini}></img>
+      <div className={toggleBurgerClass()} onClick={handleBurgerClick}>
+        <span className={headerStyles.burger_layer}></span>
+        <span className={headerStyles.burger_layer}></span>
+        <span className={headerStyles.burger_layer}></span>
       </div>
     </header>
   )
